ef204789700e36777c59a4322416954c29e1a06d
shan18u <shivanshchhabra18@gmail.com>
2022-12-23 20:01:08 -0800

    added custom_css_element.

--- align.js
+++ align.js
@@ -1,3 +1,15 @@
 // Url
 const site = window.location.hostname
 
+//Create css_custome function
+const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
+
+// Function custom_elements
+
+function Create_Element(tag,attr_tag, attr_name, vale){
+    const custom_elements = document.createElement(tag)
+    custom_elements.setAttrivute(attr_tag, attr_name)
+    custom_elements.innerHTML = value
+    document.body.append(custom_elements)
+}
+
